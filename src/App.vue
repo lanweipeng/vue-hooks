9c9691fb22a95d2ec84c78832b431fc667173243
兰为鹏 <1462191066@qq.com>
2020-10-01 13:07:27 +0800

    loading

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <useAysnc />
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import useAysnc from './example/useAsync.vue'
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,useAysnc
+  }
+}
+</script>
---
+++ src/package/useAsync.vue
@@ -0,0 +1,12 @@
+<script>
+import { ref } from "vue";
+export  default function useAsync(server) {
+  const loading = ref(true);
+  server().then(() => {
+    loading.value = false;
+  });
+  return {
+    loading,
+  };
+}
+</script>
